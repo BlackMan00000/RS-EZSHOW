a77e78fef71d83d3560a54be78b1bbd1a99e63e1
BlackMan00000 <108239538+BlackMan00000@users.noreply.github.com>
2023-11-07 19:04:51 -0800

    Add files via upload

--- RS-EZSHOW.js
+++ RS-EZSHOW.js
@@ -1,3 +1,81 @@
+// FACTS 1 ARea- 2 Slideshows START
+document.addEventListener("DOMContentLoaded", function() {
+    let SHOW = 1;
+    const slideshows = {
+        1: {
+            images: [
+                'https://matt.moses.name/slides/TempMap.png',
+                'https://matt.moses.name/slides/TempGraph1.png',
+                'https://matt.moses.name/slides/TempGraph2.png',
+                // Add more image paths for slideshow 1
+            ],
+            texts: [
+                 "The African savannahs feature vast grasslands with sporadic trees, thriving in well-drained, nutrient-rich soils. However, overgrazing, deforestation, and poor land management can lead to desertification, turning fertile land into barren desert. A healthy soil ecosystem comprises: 1) Organic matter providing nutrients; 2) Soil microbes, including bacteria and fungi, supporting decomposition and nutrient cycling; and 3) Soil structure, ensuring water retention and root penetration.",
+                "Desertification intensifies the dry season, leading to prolonged droughts, reduced water availability, and harsher conditions. This exacerbates soil degradation, diminishes vegetation cover, and disrupts ecosystems. As a result, the natural resilience of the environment decreases, making recovery during wet seasons more difficult and furthering the desertification cycle.",
+                "After full desertification, a landscape transforms into barren, arid terrain with minimal vegetation. Fertile soils become compacted, eroded, or sandy, losing their productivity. Water sources like lakes and rivers may dry up. Native wildlife diminishes or migrates elsewhere. Once-lush regions become reminiscent of deserts, with shifting sand dunes, cracked ground, and limited biodiversity.",
+        // Add more texts
+            ]
+        },
+    }
+function initializeSlideshow() {
+    let currentIndex = 0;
+    const imageDisplay = document.getElementById('imageDisplay');
+    const textDisplay = document.getElementById('textDisplay');
+    const backButton = document.getElementById('backButton');
+    const forwardButton = document.getElementById('forwardButton');
+
+    const images = slideshows[SHOW].images;
+    const texts = slideshows[SHOW].texts;
+
+    function updateDisplay() {
+        // Crossfade the images and texts
+        imageDisplay.classList.add('hidden');
+        textDisplay.classList.add('hidden');
+
+        setTimeout(() => {
+            imageDisplay.style.backgroundImage = "url(" + images[currentIndex] + ")";
+            textDisplay.textContent = texts[currentIndex];
+            imageDisplay.classList.remove('hidden');
+            textDisplay.classList.remove('hidden');
+        }, 500); // Half of the 1s transition time
+
+
+        backButton.disabled = currentIndex === 0;
+        forwardButton.disabled = currentIndex === images.length - 1 || currentIndex === texts.length - 1;
+    }
+
+    backButton.addEventListener('click', function() {
+        ButtonPress1.play();
+        if (currentIndex > 0) {
+            currentIndex--;
+            updateDisplay();
+        }
+    });
+
+    forwardButton.addEventListener('click', function() {
+        ButtonPress2.play();
+        if (currentIndex < images.length - 1 && currentIndex < texts.length - 1) {
+            currentIndex++;
+            updateDisplay();
+        }
+    });   
+
+    updateDisplay();
+    
+   }
+
+   const show2Button = document.getElementById('downToFacts2');
+    show2Button.addEventListener('click', function() {
+        btnSounds.play('ArrowFAST');
+        setTimeout(() => {
+        SHOW = 1;
+        initializeSlideshow();
+        },800 );
+    });
+    initializeSlideshow();
+});   
+
+// Slides Area
 // ******************************************
 // RS-EZSHOW - random inage selection/slider
 // ******************************************    
@@ -7,10 +85,10 @@ const button2 = document.getElementById("q2btn");
 const button3 = document.getElementById("q3btn");
 const button4 = document.getElementById("q4btn");
 const slider = document.querySelector(".rs-slider");
-const imagesArray1 = ["https://matt.moses.name/test/Q1_Slide01.png", "https://matt.moses.name/test/Q1_Slide02.png", "https://matt.moses.name/test/Q1_Slide03.png", "https://matt.moses.name/test/Q1_Slide04.png", "https://matt.moses.name/test/Q1_Slide05.png", "https://matt.moses.name/test/Q1_Slide06.png", "https://matt.moses.name/test/Q1_Slide07.png", "https://matt.moses.name/test/Q1_Slide08.png", "https://matt.moses.name/test/Q1_Slide09.png", "https://matt.moses.name/test/Q1_Slide10.png"];
-const imagesArray2 = ["https://matt.moses.name/test/Q2_Slide01.png", "https://matt.moses.name/test/Q2_Slide02.png", "https://matt.moses.name/test/Q2_Slide03.png", "https://matt.moses.name/test/Q2_Slide04.png", "https://matt.moses.name/test/Q2_Slide05.png", "https://matt.moses.name/test/Q2_Slide06.png", "https://matt.moses.name/test/Q2_Slide07.png", "https://matt.moses.name/test/Q2_Slide08.png", "https://matt.moses.name/test/Q2_Slide09.png", "https://matt.moses.name/test/Q2_Slide10.png"];
-const imagesArray3 = ["https://matt.moses.name/test/Q3_Slide01.png", "https://matt.moses.name/test/Q3_Slide02.png", "https://matt.moses.name/test/Q3_Slide03.png", "https://matt.moses.name/test/Q3_Slide04.png"];
-const imagesArray4 = ["https://matt.moses.name/test/Q4_Slide01.png", "https://matt.moses.name/test/Q4_Slide02.png", "https://matt.moses.name/test/Q4_Slide03.png", "https://matt.moses.name/test/Q4_Slide04.png"];
+const imagesArray1 = ["https://matt.moses.name/sites/P10/slides/P10_Set1A.png", "https://matt.moses.name/sites/P10/slides/P10_Set1B.png", "https://matt.moses.name/sites/P10/slides/P10_Set1C.png"];
+const imagesArray2 = ["https://matt.moses.name/sites/P10/slides/P10_Set2A.png", "https://matt.moses.name/sites/P10/slides/P10_Set2B.png", "https://matt.moses.name/sites/P10/slides/P10_Set2C.png", "https://matt.moses.name/sites/P10/slides/P10_Set2D.png"];
+const imagesArray3 = ["https://matt.moses.name/sites/P10/slides/P10_Set3A.png", "https://matt.moses.name/sites/P10/slides/P10_Set3B.png", "https://matt.moses.name/sites/P10/slides/P10_Set3C.png"];
+const imagesArray4 = ["https://matt.moses.name/sites/P10/slides/P10_Set4A.png", "https://matt.moses.name/sites/P10/slides/P10_Set4B.png", "https://matt.moses.name/sites/P10/slides/P10_Set4C.png"];
 
 let previousImages = [];
 
