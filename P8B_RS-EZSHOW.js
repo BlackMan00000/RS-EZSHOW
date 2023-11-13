85152432c3c1131b684671bd25bf69bb89d987a7
BlackMan00000 <108239538+BlackMan00000@users.noreply.github.com>
2023-11-12 16:24:41 -0800

    Add files via upload

---
+++ P8B_RS-EZSHOW.js
@@ -0,0 +1,191 @@
+// FACTS 1 ARea- 2 Slideshows START
+document.addEventListener("DOMContentLoaded", function() {
+    let SHOW = 1;
+    const slideshows = {
+        1: {
+            images: [
+                'https://matt.moses.name/slides/TempMap.png',
+                'https://matt.moses.name/slides/TempGraph1.png',
+                'https://matt.moses.name/slides/TempGraph2.png',
+                // Add more image paths for slideshow 1
+            ],
+            texts: [
+                 "The African savannahs feature vast grasslands with sporadic trees, thriving in well-drained, nutrient-rich soils. However, overgrazing, deforestation, and poor land management can lead to desertification, turning fertile land into barren desert. A healthy soil ecosystem comprises: 1) Organic matter providing nutrients; 2) Soil microbes, including bacteria and fungi, supporting decomposition and nutrient cycling; and 3) Soil structure, ensuring water retention and root penetration.",
+                "Desertification intensifies the dry season, leading to prolonged droughts, reduced water availability, and harsher conditions. This exacerbates soil degradation, diminishes vegetation cover, and disrupts ecosystems. As a result, the natural resilience of the environment decreases, making recovery during wet seasons more difficult and furthering the desertification cycle.",
+                "After full desertification, a landscape transforms into barren, arid terrain with minimal vegetation. Fertile soils become compacted, eroded, or sandy, losing their productivity. Water sources like lakes and rivers may dry up. Native wildlife diminishes or migrates elsewhere. Once-lush regions become reminiscent of deserts, with shifting sand dunes, cracked ground, and limited biodiversity.",
+        // Add more texts
+            ]
+        },
+    }
+function initializeSlideshow() {
+    let currentIndex = 0;
+    const imageDisplay = document.getElementById('imageDisplay');
+    const textDisplay = document.getElementById('textDisplay');
+    const backButton = document.getElementById('backButton');
+    const forwardButton = document.getElementById('forwardButton');
+
+    const images = slideshows[SHOW].images;
+    const texts = slideshows[SHOW].texts;
+
+    function updateDisplay() {
+        // Crossfade the images and texts
+        imageDisplay.classList.add('hidden');
+        textDisplay.classList.add('hidden');
+
+        setTimeout(() => {
+            imageDisplay.style.backgroundImage = "url(" + images[currentIndex] + ")";
+            textDisplay.textContent = texts[currentIndex];
+            imageDisplay.classList.remove('hidden');
+            textDisplay.classList.remove('hidden');
+        }, 500); // Half of the 1s transition time
+
+
+        backButton.disabled = currentIndex === 0;
+        forwardButton.disabled = currentIndex === images.length - 1 || currentIndex === texts.length - 1;
+    }
+
+    backButton.addEventListener('click', function() {
+        ButtonPress1.play();
+        if (currentIndex > 0) {
+            currentIndex--;
+            updateDisplay();
+        }
+    });
+
+    forwardButton.addEventListener('click', function() {
+        ButtonPress2.play();
+        if (currentIndex < images.length - 1 && currentIndex < texts.length - 1) {
+            currentIndex++;
+            updateDisplay();
+        }
+    });   
+
+    updateDisplay();
+    
+   }
+
+   const show2Button = document.getElementById('downToFacts2');
+    show2Button.addEventListener('click', function() {
+        btnSounds.play('ArrowFAST');
+        setTimeout(() => {
+        SHOW = 1;
+        initializeSlideshow();
+        },800 );
+    });
+    initializeSlideshow();
+});   
+
+// Slides Area
+// ******************************************
+// RS-EZSHOW - random inage selection/slider
+// ******************************************    
+    
+const button1 = document.getElementById("q1btn"); // Just Match up the button id's
+const button2 = document.getElementById("q2btn");
+const button3 = document.getElementById("q3btn");
+const button4 = document.getElementById("q4btn");
+const slider = document.querySelector(".rs-slider");
+const imagesArray1 = ["https://matt.moses.name/sites/P8B/slides/P8B_Set1A.png", "https://matt.moses.name/sites/P8B/slides/P8B_Set1B.png", "https://matt.moses.name/sites/P8B/slides/P8B_Set1C.png"];
+const imagesArray2 = ["https://matt.moses.name/sites/P8B/slides/P8B_Set2A.png", "https://matt.moses.name/sites/P8B/slides/P8B_Set2B.png", "https://matt.moses.name/sites/P8B/slides/P8B_Set2C.png"];
+const imagesArray3 = ["https://matt.moses.name/sites/P8B/slides/P8B_Set3A.png", "https://matt.moses.name/sites/P8B/slides/P8B_Set3B.png"];
+const imagesArray4 = ["https://matt.moses.name/sites/P8B/slides/P8B_Set4A.png"];
+
+let previousImages = [];
+
+
+// This function picks randomly from the imageArrays, 
+// and does not pick the same slide twice until all have been shown
+
+function getRandomImage(imagesArray) {
+  const unusedImages = imagesArray.filter(image => !previousImages.includes(image));
+  if (unusedImages.length === 0) {
+    previousImages = [];
+    return imagesArray[Math.floor(Math.random() * imagesArray.length)];
+  }
+  return unusedImages[Math.floor(Math.random() * unusedImages.length)];
+}
+
+
+// ******************************************************************************
+// This slides the images according to RS-EZShowStyles.css stylesheet parameters
+// ******************************************************************************
+
+function updateSliderWithAnimation(incomingImage) {
+  const currentImage = document.querySelector(".rs-slider img");
+  const newImage = document.createElement("img");
+  newImage.src = incomingImage;
+  newImage.alt = "Slide";
+  newImage.classList.add("slide");
+  slider.appendChild(newImage);
+
+  newImage.style.animation = "slideInAnimation 3s ease-in-out";
+  currentImage.style.animation = "slideOutAnimation 3s ease-in-out"; //timing/easing
+
+  setTimeout(() => {
+    slider.removeChild(currentImage);
+  }, 3000); // Make sure timings match to above seconds
+}
+
+// **************************************************
+//    What Happens When you press the buttons
+// **************************************************
+
+button1.addEventListener("click", () => {
+  btnSounds.play('Machine');
+  const image = getRandomImage(imagesArray1);
+  previousImages.push(image);
+  updateSliderWithAnimation(image);
+   document.getElementById("q2btn").style.visibility = "hidden"; //2
+   document.getElementById("q3btn").style.visibility = "hidden"; //3
+   document.getElementById("q4btn").style.visibility = "hidden"; //4
+    setTimeout(function() {
+        document.getElementById("q2btn").style.visibility = "visible"; //2
+        document.getElementById("q3btn").style.visibility = "visible"; //3
+        document.getElementById("q4btn").style.visibility = "visible"; //4
+    }, 3000);
+});
+
+button2.addEventListener("click", () => {
+    btnSounds.play('Machine');
+  const image = getRandomImage(imagesArray2);
+  previousImages.push(image);
+  updateSliderWithAnimation(image);
+  document.getElementById("q1btn").style.visibility = "hidden"; //1
+   document.getElementById("q3btn").style.visibility = "hidden"; //3
+   document.getElementById("q4btn").style.visibility = "hidden"; //4
+    setTimeout(function() {
+        document.getElementById("q1btn").style.visibility = "visible"; //1
+        document.getElementById("q3btn").style.visibility = "visible"; //3
+        document.getElementById("q4btn").style.visibility = "visible"; //4
+    }, 3000);
+});
+
+button3.addEventListener("click", () => {
+  btnSounds.play('Machine');
+  const image = getRandomImage(imagesArray3);
+  previousImages.push(image);
+  updateSliderWithAnimation(image);
+  document.getElementById("q2btn").style.visibility = "hidden"; //2
+   document.getElementById("q1btn").style.visibility = "hidden"; //1
+   document.getElementById("q4btn").style.visibility = "hidden"; //4
+    setTimeout(function() {
+        document.getElementById("q2btn").style.visibility = "visible"; //2
+        document.getElementById("q1btn").style.visibility = "visible"; //1
+        document.getElementById("q4btn").style.visibility = "visible"; //4
+    }, 3000);
+});
+
+button4.addEventListener("click", () => {
+    btnSounds.play('Machine');
+  const image = getRandomImage(imagesArray4);
+  previousImages.push(image);
+  updateSliderWithAnimation(image);
+  document.getElementById("q2btn").style.visibility = "hidden"; //2
+   document.getElementById("q3btn").style.visibility = "hidden"; //3
+   document.getElementById("q1btn").style.visibility = "hidden"; //1
+    setTimeout(function() {
+        document.getElementById("q2btn").style.visibility = "visible"; //2
+        document.getElementById("q3btn").style.visibility = "visible"; //3
+        document.getElementById("q1btn").style.visibility = "visible"; //1
+    }, 3000);
+});
