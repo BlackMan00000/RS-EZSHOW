822ba00f59633dc322effa0953fa5d93a59e33f6
BlackMan00000 <108239538+BlackMan00000@users.noreply.github.com>
2023-11-13 21:35:51 -0800

    Update P10_RS-EZSHOW.js

--- P10_RS-EZSHOW.js
+++ P10_RS-EZSHOW.js
@@ -4,13 +4,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const slideshows = {
         1: {
             images: [
-                'https://matt.moses.name/slides/TempMap.png',
+                'https://matt.moses.name/sites/P10/facts1/P10_facts1_A.jpg',
                 'https://matt.moses.name/slides/TempGraph1.png',
                 'https://matt.moses.name/slides/TempGraph2.png',
                 // Add more image paths for slideshow 1
             ],
             texts: [
-                 "The African savannahs feature vast grasslands with sporadic trees, thriving in well-drained, nutrient-rich soils. However, overgrazing, deforestation, and poor land management can lead to desertification, turning fertile land into barren desert. A healthy soil ecosystem comprises: 1) Organic matter providing nutrients; 2) Soil microbes, including bacteria and fungi, supporting decomposition and nutrient cycling; and 3) Soil structure, ensuring water retention and root penetration.",
+                 "To stop rhino horn poaching, a complex solution is necessary that combines enhanced surveillance and law enforcement in conservation areas with community engagement and education to reduce economic incentives for poaching. International collaboration and stringent legislation are crucial to curb the demand and trade of rhino horns. Investing in scientific research and advanced technologies for tracking and monitoring rhino populations, along with establishing wildlife sanctuaries and relocation programs, can provide safer habitats. Additionally, global public awareness campaigns are vital to debunk myths about rhino horns and promote wildlife conservation ethics, contributing significantly to the protection of rhinos.",
                 "Desertification intensifies the dry season, leading to prolonged droughts, reduced water availability, and harsher conditions. This exacerbates soil degradation, diminishes vegetation cover, and disrupts ecosystems. As a result, the natural resilience of the environment decreases, making recovery during wet seasons more difficult and furthering the desertification cycle.",
                 "After full desertification, a landscape transforms into barren, arid terrain with minimal vegetation. Fertile soils become compacted, eroded, or sandy, losing their productivity. Water sources like lakes and rivers may dry up. Native wildlife diminishes or migrates elsewhere. Once-lush regions become reminiscent of deserts, with shifting sand dunes, cracked ground, and limited biodiversity.",
         // Add more texts
