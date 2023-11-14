b865e87f9abab0ec555c9f5609185efce364c011
BlackMan00000 <108239538+BlackMan00000@users.noreply.github.com>
2023-11-14 13:18:09 -0800

    Create P2B_RS-EZSHOW.js

---
+++ P2B_RS-EZSHOW.js
@@ -0,0 +1,191 @@
+// FACTS 1 ARea- 2 Slideshows START
+document.addEventListener("DOMContentLoaded", function() {
+    let SHOW = 1;
+    const slideshows = {
+        1: {
+            images: [
+                'https://matt.moses.name/sites/P2B/facts1/P2B_facts1_A.jpg',
+                'https://matt.moses.name/sites/P2B/facts1/P2B_facts1_B.jpg',
+                'https://matt.moses.name/slides/TempGraph2.png',
+                // Add more image paths for slideshow 1
+            ],
+            texts: [
+                "The Cretaceous period, from about 145 to 66 million years ago, featured a rich variety of flora. This era was notable for the significant rise of flowering plants (angiosperms), which rapidly diversified and became dominant in many ecosystems, introducing a range of shapes, sizes, and colors. Gymnosperms such as conifers, cycads, and ginkgoes continued to be important, although their dominance waned compared to earlier periods. Ferns and horsetails were also widespread, forming dense undergrowths in forests. This diverse vegetation supported various herbivorous dinosaurs and significantly influenced the ecosystems. The period's end, marked by a mass extinction, profoundly altered plant life, especially favoring the evolution and dominance of flowering plants.",
+                "Volcanism played a significant role in the demise of the dinosaurs through massive eruptions in the Deccan Traps, a large volcanic province in modern-day India. These eruptions, occurring roughly 66 million years ago, coincided with the Cretaceous-Paleogene (K-Pg) extinction event. The vast quantities of lava, ash, and gas, especially sulfur and carbon dioxide, released into the atmosphere had profound climatic impacts. This led to substantial cooling, acid rain, and disrupted ecosystems, weakening dinosaur populations and making them more vulnerable to other catastrophic events, like the Chicxulub asteroid impact.",
+                "After full desertification, a landscape transforms into barren, arid terrain with minimal vegetation. Fertile soils become compacted, eroded, or sandy, losing their productivity. Water sources like lakes and rivers may dry up. Native wildlife diminishes or migrates elsewhere. Once-lush regions become reminiscent of deserts, with shifting sand dunes, cracked ground, and limited biodiversity.",
+        // Add more texts
+            ]
+        },
+    }
+function initializeSlideshow() {
+    let currentIndex = 0;
+    const imageDisplay = document.getElementById('imageDisplay');
+    const textDisplay = document.getElementById('textDisplay');
+    const backButton = document.getElementById('backButton');
+    const forwardButton = document.getElementById('forwardButton');
+
+    const images = slideshows[SHOW].images;
+    const texts = slideshows[SHOW].texts;
+
+    function updateDisplay() {
+        // Crossfade the images and texts
+        imageDisplay.classList.add('hidden');
+        textDisplay.classList.add('hidden');
+
+        setTimeout(() => {
+            imageDisplay.style.backgroundImage = "url(" + images[currentIndex] + ")";
+            textDisplay.textContent = texts[currentIndex];
+            imageDisplay.classList.remove('hidden');
+            textDisplay.classList.remove('hidden');
+        }, 500); // Half of the 1s transition time
+
+
+        backButton.disabled = currentIndex === 0;
+        forwardButton.disabled = currentIndex === images.length - 1 || currentIndex === texts.length - 1;
+    }
+
+    backButton.addEventListener('click', function() {
+        ButtonPress1.play();
+        if (currentIndex > 0) {
+            currentIndex--;
+            updateDisplay();
+        }
+    });
+
+    forwardButton.addEventListener('click', function() {
+        ButtonPress2.play();
+        if (currentIndex < images.length - 1 && currentIndex < texts.length - 1) {
+            currentIndex++;
+            updateDisplay();
+        }
+    });   
+
+    updateDisplay();
+    
+   }
+
+   const show2Button = document.getElementById('downToFacts2');
+    show2Button.addEventListener('click', function() {
+        btnSounds.play('ArrowFAST');
+        setTimeout(() => {
+        SHOW = 1;
+        initializeSlideshow();
+        },800 );
+    });
+    initializeSlideshow();
+});   
+
+// Slides Area
+// ******************************************
+// RS-EZSHOW - random inage selection/slider
+// ******************************************    
+    
+const button1 = document.getElementById("q1btn"); // Just Match up the button id's
+const button2 = document.getElementById("q2btn");
+const button3 = document.getElementById("q3btn");
+const button4 = document.getElementById("q4btn");
+const slider = document.querySelector(".rs-slider");
+const imagesArray1 = ["https://matt.moses.name/sites/P2A/slides/P2AB_Set2A.png", "https://matt.moses.name/sites/P2A/slides/P2AB_Set1B.png"];
+const imagesArray2 = ["https://matt.moses.name/sites/P2A/slides/P2AB_Set2A.png"];
+const imagesArray3 = ["https://matt.moses.name/sites/P2A/slides/P2AB_Set3A.png", "https://matt.moses.name/sites/P2A/slides/P2AB_Set3B.png"];
+const imagesArray4 = ["https://matt.moses.name/sites/P2A/slides/P2AB_Set4A.png"];
+
+let previousImages = [];
+
+
+// This function picks randomly from the imageArrays, 
+// and does not pick the same slide twice until all have been shown
+
+function getRandomImage(imagesArray) {
+  const unusedImages = imagesArray.filter(image => !previousImages.includes(image));
+  if (unusedImages.length === 0) {
+    previousImages = [];
+    return imagesArray[Math.floor(Math.random() * imagesArray.length)];
+  }
+  return unusedImages[Math.floor(Math.random() * unusedImages.length)];
+}
+
+
+// ******************************************************************************
+// This slides the images according to RS-EZShowStyles.css stylesheet parameters
+// ******************************************************************************
+
+function updateSliderWithAnimation(incomingImage) {
+  const currentImage = document.querySelector(".rs-slider img");
+  const newImage = document.createElement("img");
+  newImage.src = incomingImage;
+  newImage.alt = "Slide";
+  newImage.classList.add("slide");
+  slider.appendChild(newImage);
+
+  newImage.style.animation = "slideInAnimation 3s ease-in-out";
+  currentImage.style.animation = "slideOutAnimation 3s ease-in-out"; //timing/easing
+
+  setTimeout(() => {
+    slider.removeChild(currentImage);
+  }, 3000); // Make sure timings match to above seconds
+}
+
+// **************************************************
+//    What Happens When you press the buttons
+// **************************************************
+
+button1.addEventListener("click", () => {
+  btnSounds.play('Machine');
+  const image = getRandomImage(imagesArray1);
+  previousImages.push(image);
+  updateSliderWithAnimation(image);
+   document.getElementById("q2btn").style.visibility = "hidden"; //2
+   document.getElementById("q3btn").style.visibility = "hidden"; //3
+   document.getElementById("q4btn").style.visibility = "hidden"; //4
+    setTimeout(function() {
+        document.getElementById("q2btn").style.visibility = "visible"; //2
+        document.getElementById("q3btn").style.visibility = "visible"; //3
+        document.getElementById("q4btn").style.visibility = "visible"; //4
+    }, 3000);
+});
+
+button2.addEventListener("click", () => {
+	btnSounds.play('Machine');
+  const image = getRandomImage(imagesArray2);
+  previousImages.push(image);
+  updateSliderWithAnimation(image);
+  document.getElementById("q1btn").style.visibility = "hidden"; //1
+   document.getElementById("q3btn").style.visibility = "hidden"; //3
+   document.getElementById("q4btn").style.visibility = "hidden"; //4
+    setTimeout(function() {
+        document.getElementById("q1btn").style.visibility = "visible"; //1
+        document.getElementById("q3btn").style.visibility = "visible"; //3
+        document.getElementById("q4btn").style.visibility = "visible"; //4
+    }, 3000);
+});
+
+button3.addEventListener("click", () => {
+  btnSounds.play('Machine');
+  const image = getRandomImage(imagesArray3);
+  previousImages.push(image);
+  updateSliderWithAnimation(image);
+  document.getElementById("q2btn").style.visibility = "hidden"; //2
+   document.getElementById("q1btn").style.visibility = "hidden"; //1
+   document.getElementById("q4btn").style.visibility = "hidden"; //4
+    setTimeout(function() {
+        document.getElementById("q2btn").style.visibility = "visible"; //2
+        document.getElementById("q1btn").style.visibility = "visible"; //1
+        document.getElementById("q4btn").style.visibility = "visible"; //4
+    }, 3000);
+});
+
+button4.addEventListener("click", () => {
+	btnSounds.play('Machine');
+  const image = getRandomImage(imagesArray4);
+  previousImages.push(image);
+  updateSliderWithAnimation(image);
+  document.getElementById("q2btn").style.visibility = "hidden"; //2
+   document.getElementById("q3btn").style.visibility = "hidden"; //3
+   document.getElementById("q1btn").style.visibility = "hidden"; //1
+    setTimeout(function() {
+        document.getElementById("q2btn").style.visibility = "visible"; //2
+        document.getElementById("q3btn").style.visibility = "visible"; //3
+        document.getElementById("q1btn").style.visibility = "visible"; //1
+    }, 3000);
+});
