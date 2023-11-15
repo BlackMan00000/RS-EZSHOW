b0fc4431b7c2a4946c7085fabde1fb61c6dc15a3
BlackMan00000 <108239538+BlackMan00000@users.noreply.github.com>
2023-11-15 07:58:27 -0800

    Update P4_RS-EZSHOW.js

--- P4_RS-EZSHOW.js
+++ P4_RS-EZSHOW.js
@@ -4,16 +4,17 @@ document.addEventListener("DOMContentLoaded", function() {
     const slideshows = {
         1: {
             images: [
-                'https://matt.moses.name/sites/P4/facts1/P4_facts1_A.jpg',
-                'https://matt.moses.name/slides/TempGraph1.png',
-                'https://matt.moses.name/slides/TempGraph2.png',
+                'facts1/P4_facts1_A.jpg',
+                'facts1/P4_facts1_B.jpg',
+                'facts1/P4_facts1_C.jpg',
                 // Add more image paths for slideshow 1
             ],
             texts: [
                  "The Ice Age, also known as the Pleistocene Epoch, was not uniformly cold and frozen everywhere. It was characterized by significant glaciations, where large parts of the Earth, particularly in the Northern Hemisphere, were covered by ice sheets. However, not all regions experienced the same degree of coldness or ice coverage. Areas closer to the equator remained relatively warmer and ice-free, sustaining a variety of plant and animal life. The climate varied significantly across different regions and periods, with some experiencing milder conditions. Moreover, there were interglacial periods within the Ice Age when temperatures were relatively warmer, leading to the retreat of ice sheets. This variation in climate and ice coverage indicates that the Ice Age was a complex and dynamic period, rather than a uniformly frozen and cold epoch.",
-                "Desertification intensifies the dry season, leading to prolonged droughts, reduced water availability, and harsher conditions. This exacerbates soil degradation, diminishes vegetation cover, and disrupts ecosystems. As a result, the natural resilience of the environment decreases, making recovery during wet seasons more difficult and furthering the desertification cycle.",
-                "After full desertification, a landscape transforms into barren, arid terrain with minimal vegetation. Fertile soils become compacted, eroded, or sandy, losing their productivity. Water sources like lakes and rivers may dry up. Native wildlife diminishes or migrates elsewhere. Once-lush regions become reminiscent of deserts, with shifting sand dunes, cracked ground, and limited biodiversity.",
-        // Add more texts
+                 "During the Last Ice Age, massive ice sheets covered significant parts of the Earth, locking up vast quantities of water. This led to lower sea levels, about 120 meters below today's levels. The Bering Strait, a relatively shallow body of water between modern-day Russia and Alaska, was exposed due to this drop in sea level, forming a land bridge. This bridge connected Asia and North America, allowing humans and animals to migrate across what is now known as Beringia. As the ice age ended and the ice melted, sea levels rose again, submerging the land bridge.",
+                 "During the Last Ice Age, glaciers covered a significant portion of North America. In the United States, they extended as far south as present-day Missouri and Illinois in the Midwest, and New York and Pennsylvania in the East. These massive ice sheets sculpted much of the current landscape, forming the Great Lakes and shaping valleys and mountains. As the climate warmed, the glaciers retreated, leaving behind a reshaped terrain, numerous lakes, and fertile soil in the northern U.S.",
+
+                // Add more texts
             ]
         },
     }
@@ -85,10 +86,10 @@ const button2 = document.getElementById("q2btn");
 const button3 = document.getElementById("q3btn");
 const button4 = document.getElementById("q4btn");
 const slider = document.querySelector(".rs-slider");
-const imagesArray1 = ["https://matt.moses.name/sites/P4/slides/P4_Set1A.png", "https://matt.moses.name/sites/P4/slides/P4_Set1B.png", "https://matt.moses.name/sites/P4/slides/P4_Set1C.png"];
-const imagesArray2 = ["https://matt.moses.name/sites/P4/slides/P4_Set2A.png", "https://matt.moses.name/sites/P4/slides/P4_Set2B.png"];
-const imagesArray3 = ["https://matt.moses.name/sites/P4/slides/P4_Set3A.png", "https://matt.moses.name/sites/P4/slides/P4_Set3B.png", "https://matt.moses.name/sites/P4/slides/P4_Set3C.png"];
-const imagesArray4 = ["https://matt.moses.name/sites/P4/slides/P4_Set4A.png"];
+const imagesArray1 = ["slides/P4_Set1A.png", "slides/P4_Set1B.png", "slides/P4_Set1C.png"];
+const imagesArray2 = ["slides/P4_Set2A.png", "slides/P4_Set2B.png"];
+const imagesArray3 = ["slides/P4_Set3A.png", "slides/P4_Set3B.png", "slides/P4_Set3C.png"];
+const imagesArray4 = ["slides/P4_Set4A.png"];
 
 let previousImages = [];
 
@@ -146,7 +147,7 @@ button1.addEventListener("click", () => {
 });
 
 button2.addEventListener("click", () => {
-	btnSounds.play('Machine');
+    btnSounds.play('Machine');
   const image = getRandomImage(imagesArray2);
   previousImages.push(image);
   updateSliderWithAnimation(image);
@@ -176,7 +177,7 @@ button3.addEventListener("click", () => {
 });
 
 button4.addEventListener("click", () => {
-	btnSounds.play('Machine');
+    btnSounds.play('Machine');
   const image = getRandomImage(imagesArray4);
   previousImages.push(image);
   updateSliderWithAnimation(image);
